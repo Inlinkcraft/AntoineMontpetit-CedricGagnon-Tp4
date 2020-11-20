9b4d6909b7c9ad09b3b1b2c81a2fbfe529f2dd8a
DiamondCed <74017882+DiamondCed@users.noreply.github.com>
2020-11-20 14:57:41 -0500

    added things

---
+++ AntoineMontpetit-CedricGagnon - TP4/src/utilitaires/Input.java
@@ -0,0 +1,106 @@
+/**
+ * @author Jocelyn -- Cégep Limoilou
+ *
+ * @version 2013
+ */
+package utilitaires;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Cette classe offre certains outils utiles.
+ *
+ */
+public class Input
+{
+	/**
+	 * Saisir un entier. La saisie est non bloquante.
+	 *
+	 * @param pQuestion chaîne permettant de poser une question.
+	 *
+	 * @return Integer, un objet entier (Integer) permettant de retourner la
+	 *         valeur ou "null" si l'utilisateur a décidé d'annuler la saisie.
+	 *
+	 */
+	public static Integer saisirEntier(String pQuestion)
+	{
+		Integer objetEntier = null;
+		boolean valide = false;
+		String reponse = "";
+
+		// Saisir tant que pas valide
+		do
+		{
+			// Boîte de dialogue de saisie
+			reponse = JOptionPane.showInputDialog(null, pQuestion);
+
+			if (reponse != null)
+			{
+				try
+				{
+					objetEntier = new Integer(Integer.parseInt(reponse));
+					valide = true;
+				}
+				catch (Exception e)
+				{
+					valide = false;
+				}
+			}
+		}
+		while (!valide && reponse != null);
+
+		return objetEntier;
+	}
+
+	/**
+	 * Saisir une chaîne de caractères. La saisie est non bloquante.
+	 *
+	 * @param pQuestion chaîne permettant de poser une question.
+	 *
+	 * @return String un objet chaîne de caractères permettant de retourner la
+	 *         valeur ou "null" si l'utilisateur a décidé d'annuler la saisie.
+	 */
+	public static String saisirString(String pQuestion)
+	{
+		String stringTemp = null;
+		boolean valide = false;
+		String reponse = "";
+
+		// Saisir tant que pas valide
+		do
+		{
+			// Boîte de dialogue de saisie
+			reponse = JOptionPane.showInputDialog(null, pQuestion);
+
+			if (reponse != null)
+			{
+				stringTemp = reponse;
+				valide = true;
+			}
+		}
+		while (!valide && reponse != null);
+
+		return stringTemp;
+	}
+
+	/**
+	 * Permet d'afficher une question (message) dans une boîte de confirmation
+	 * avec les boutons oui et non
+	 *
+	 * @param pQuestion
+	 * @return
+	 */
+	public static boolean confirmerMessage(String pQuestion)
+	{
+		boolean confirme = true;
+
+		// Boîte de dialogue d'affichage
+		if (JOptionPane.NO_OPTION == JOptionPane.showConfirmDialog(null,
+				pQuestion, "Confirmation", JOptionPane.YES_NO_OPTION))
+		{
+			confirme = false;
+		}
+
+		return confirme;
+	}
+}
